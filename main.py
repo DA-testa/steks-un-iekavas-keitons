ad874c35a3d2a54e0d006f2c2e906c4d4e1f9c0b
keitons <125758836+keitons@users.noreply.github.com>
2023-02-20 13:06:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 # python3
-
-class Bracket:
-    def _init_(self, char, position):
-        self.char = char
-        self.position = position
-
+BRACKETS_MAP = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+}
 
 def are_matching(left, right):
-    matching_pairs = {"(": ")", "[": "]", "{": "}"}
-    return left in matching_pairs and right == matching_pairs[left]
-
+    return BRACKETS_MAP.get(left) == right
 
 def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next_char in enumerate(text):
-        if next_char in "([{":
-            opening_brackets_stack.append(Bracket(next_char, i))
-        elif next_char in ")]}":
-            if not opening_brackets_stack:
-                return i + 1
-            top = opening_brackets_stack[-1]
-            if not are_matching(top.char, next_char):
-                return i + 1
-            opening_brackets_stack.pop()
-    if opening_brackets_stack:
-        return opening_brackets_stack[0].position + 1
-    return "Success"
-
+    stack = []
+    for i, char in enumerate(text, start=1):
+        if char in BRACKETS_MAP:
+            stack.append((char, i))
+        elif char in BRACKETS_MAP.values():
+            if not stack:
+                return i
+            last_bracket, _ = stack.pop()
+            if not are_matching(last_bracket, char):
+                return i
+    if stack:
+        return stack[0][1]
+    return 'Success'
 
 def main():
     text = input()
@@ -35,7 +30,5 @@ def main():
         mismatch = find_mismatch(text)
         print(mismatch)
 
-
 if _name_ == "_main_":
     main()
-    

b133af8688cb8640fa74975a05f789255a17bb31
keitons <125758836+keitons@users.noreply.github.com>
2023-02-20 09:45:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,36 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
+    opening_brackets = ['[', '{', '(']
+    closing_brackets = [']', '}', ')']
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+    closing_brackets_stack = []
+    for i, c in enumerate(text):
+        if c in "([{":
+            stack.append(c)
+            opening_stack.append(i + 1)
+        elif c in ")]}:
+            if not stack:
+                return i + 1
+        if closing_brackets.index(c) == opening_brackets.index(stack[-1]):
+                stack.pop()
+                closing_stack.pop()
+            else:
+                return i + 1
+        else:
+            continue
 
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    if stack:
+        return closing_stack[0]
+    elif opening_stack:
+        return opening_stack[0]
+    else:
+        return "Success"
 
 
 if __name__ == "__main__":
